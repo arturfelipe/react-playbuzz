01e42d0eb28596a6002db20d9db8d054b6eba362
Artur Sousa <artur.sousa@corp.globo.com>
2017-03-19 09:12:33 -0300

    Install and config a demo page

---
+++ webpack.demo.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: {
+    'app': [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      path.join(__dirname, 'demo/index.js')
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '/demo/'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
